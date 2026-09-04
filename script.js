f1b4702a9b7e0d69513418eb625c854e1be911cc
Promise Ezeibe Chinonso <pezeibe12@gmail.com>
2026-09-04 06:33:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let incrementBtn = document.getElementById("increment-btn")
+let saveBtn = document.getElementById("save-btn")
+
+let count = 0
+
+incrementBtn.addEventListener("click", function() {
+    count = count + 1
+    countEl.innerText = count
+})
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    console.log(count)
+}
+
+saveBtn.addEventListener("click", function() {
+    save()
+})
